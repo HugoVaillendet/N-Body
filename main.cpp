48d6fb0e9a13499237a909f3305a4ca42ed733a3
HugoVaillendet <hugo.vaillendet@etu.sorbonne-universite.fr>
2026-04-03 11:28:05 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include "json.hpp"
 
-constexpr size_t N = 2;
-constexpr double G = 1.0;
-constexpr double epsilon = 1e-5;
+using json = nlohmann::json;
+
+constexpr double F_KM_M = 1e3;
+constexpr double G = 6.6743e-11;
 
 constexpr double t_0 = 0.0;
-constexpr double t_f = 100.0;
-constexpr double dt = 0.001;
+constexpr double t_f = 20.0 * 31557600.0;
+constexpr double dt = 800.0;
 
 constexpr size_t M = static_cast<size_t>((t_f - t_0) / dt);
+
+size_t N = 0;
 
 std::vector<double> t(M);
 std::vector<double> E(M);
@@ -29,20 +33,40 @@ struct Bodies {
     std::vector<double> ay;
     std::vector<double> az;
     std::vector<double> m;
-
-    void init() {
-        this->x.resize(N);
-        this->y.resize(N);
-        this->z.resize(N);
-        this->vx.resize(N);
-        this->vy.resize(N);
-        this->vz.resize(N);
-        this->ax.resize(N);
-        this->ay.resize(N);
-        this->az.resize(N);
-        this->m.resize(N);
-    }
 };
+
+void load_config(const std::string& path, Bodies& b) {
+
+    std::ifstream config(path);
+    json data = json::parse(config);
+
+    N = data["bodies"].size();
+    b.x.resize(N);
+    b.y.resize(N);
+    b.z.resize(N);
+    b.vx.resize(N);
+    b.vy.resize(N);
+    b.vz.resize(N);
+    b.ax.resize(N);
+    b.ay.resize(N);
+    b.az.resize(N);
+    b.m.resize(N);
+    
+    for (auto& body : data["bodies"]) {
+        int id = body["id"];
+
+        b.x[id]  = body["position"]["x"].get<double>() * F_KM_M;
+        b.y[id]  = body["position"]["y"].get<double>() * F_KM_M;
+        b.z[id]  = body["position"]["z"].get<double>() * F_KM_M;
+
+        b.vx[id] = body["velocity"]["vx"].get<double>() * F_KM_M;
+        b.vy[id] = body["velocity"]["vy"].get<double>() * F_KM_M;
+        b.vz[id] = body["velocity"]["vz"].get<double>() * F_KM_M;
+
+        b.m[id] = body["mass"].get<double>();
+    }
+
+}
 
 void acceleration(Bodies& b) {
 
@@ -61,17 +85,17 @@ void acceleration(Bodies& b) {
             double dz = b.z[j] - b.z[i];
 
             //Distance squared between i and j.
-            double r2 = dx*dx + dy*dy + dz*dz + epsilon*epsilon;
+            double r2 = dx*dx + dy*dy + dz*dz;
 
-            double inverse_r3 = 1.0 / (r2 * std::sqrt(r2));
+            double inverse_r3 = (r2 * std::sqrt(r2));
 
-            //Evaluate force once.
-            double F =(G * b.m[j]) * inverse_r3;
+            //Evaluate acceleration per unit of distance.
+            double A =(G * b.m[j]) / inverse_r3;
 
             //Apply acceleration on the 3 components.
-            b.ax[i] += F * dx;
-            b.ay[i] += F * dy;
-            b.az[i] += F * dz;
+            b.ax[i] += A * dx;
+            b.ay[i] += A * dy;
+            b.az[i] += A * dz;
         }
     }
 }
@@ -119,7 +143,7 @@ double energy_evaluation(Bodies& b) {
             double dz = b.z[j] - b.z[i];
 
             //Distance squared between i and j.
-            double r2 = dx*dx + dy*dy + dz*dz + epsilon*epsilon;
+            double r2 = dx*dx + dy*dy + dz*dz;
 
             //-= here since the potential is negative.
             V -= G * (b.m[i] * b.m[j]) / std::sqrt(r2);
@@ -129,110 +153,91 @@ double energy_evaluation(Bodies& b) {
     return (T + V);
 }
 
-void initialize(Bodies& b) {
+void initialize(Bodies& b, const std::string& path) {
 
-    b.init();
 
-    /*{
-        b.x[0] = -1.0;
-        b.y[0] = 0.0;
-        b.z[0] = 0.0;
-
-        b.vx[0] = 0.412103;
-        b.vy[0] = 0.283384;
-        b.vz[0] = 0.0;
-
-        b.ax[0] = 0.0;
-        b.ay[0] = 0.0;
-        b.az[0] = 0.0;
-
-        b.x[1] = 1.0;
-        b.y[1] = 0.0;
-        b.z[1] = 0.0;
-
-        b.vx[1] = 0.412103;
-        b.vy[1] = 0.283384;
-        b.vz[1] = 0.0;
-
-        b.ax[1] = 0.0;
-        b.ay[1] = 0.0;
-        b.az[1] = 0.0;
-
-        b.x[2] = 0.0;
-        b.y[2] = 0.0;
-        b.z[2] = 0.0;
-
-        b.vx[2] = -2.0 * 0.412103;
-        b.vy[2] = -2.0 * 0.283384;
-        b.vz[2] = 0.0;
-
-        b.ax[2] = 0.0;
-        b.ay[2] = 0.0;
-        b.az[2] = 0.0;
-
-        b.m[0] = 10.0;
-        b.m[1] = 10.0;
-        b.m[2] = 10.0;
-    }*/
-
-    {
-
-        b.m[0] = 10.0;
-        b.m[1] = 10.0;
-
-        b.x[0] = -50.0;
-        b.y[0] = 0.0;
-        b.z[0] = 0.0;
-
-        b.vx[0] = 0.0;
-        b.vy[0] = -0.2236;
-        b.vz[0] = 0.0;
-
-        b.ax[0] = 0.0;
-        b.ay[0] = 0.0;
-        b.az[0] = 0.0;
-
-        b.x[1] = 50.0;
-        b.y[1] = 0.0;
-        b.z[1] = 0.0;
-
-        b.vx[1] = 0.0;
-        b.vy[1] = 0.2236;
-        b.vz[1] = 0.0;
-
-        b.ax[1] = 0.0;
-        b.ay[1] = 0.0;
-        b.az[1] = 0.0;
-    }
+    load_config(path, b);
+    acceleration(b);
 
     for (int i = 0; i < M; i ++) {
         t[i] = dt * static_cast<double>(i);
     }
 
     std::cout << "Initalization successfull !\n";
+
 }
 
 void simulation(Bodies& b, std::vector<double>& t, std::vector<double>& E, 
-    const std::string& path1) {
-
-    initialize(b);
+                const std::string& path1) {
 
     std::ofstream Energy(path1);
-
+    Energy.precision(std::numeric_limits<double>::max_digits10);
+    Energy << std::scientific;
+    
+    //For each timestep we apply leapfrog integration and record energy.
     for (int i = 0; i < t.size(); i++) {
         leapfrog_integrator(b);
         E[i] = energy_evaluation(b);
-        Energy << E[i] << '\n';
+        Energy << t[i] << " " << E[i] << '\n';
     }
 
+    Energy.close();
+
     std::cout << "Simulation successful !\n";
+}
+
+std::vector<double> RG_evaluate(Bodies& b) {
+
+    std::vector<double> RG(N-1);
+    
+    for (int i = 1; i < N; i++) {
+        double dx = b.x[i] - b.x[0];
+        double dy = b.y[i] - b.y[0];
+        double dz = b.z[i] - b.z[0];
+
+        double r2 = dx*dx + dy*dy + dz*dz;
+
+        RG[i-1] = std::sqrt(r2);
+    }
+    return RG;
+}
+
+void export_data(Bodies& b, const std::string& path_in, const std::string& path_out) {
+
+    std::vector<double> RG = RG_evaluate(b);
+
+    std::ifstream config(path_in);
+    json data = json::parse(config);
+
+    std::ofstream File(path_out + ".dat");
+    File.precision(std::numeric_limits<double>::max_digits10);
+    File << std::scientific;
+
+    for (auto& body : data["bodies"]) { 
+        size_t idx = static_cast<size_t>(body["id"].get<int>());
+        File << body["name"] << '\n';
+        File << "Positions: ";
+        File << b.x[idx] << " " << b.y[idx] << " " << b.z[idx] << '\n';
+        File << "Velocities: ";
+        File << b.vx[idx] << " " << b.vy[idx] << " " << b.vz[idx] << '\n';
+        if (idx > 0) {
+            File << "RG: " << RG[idx-1] << '\n';
+        }
+
+        File << '\n';
+    }
+
+    File.close();
 }
 
 int main() {
     
     Bodies b;
+    initialize(b, "solar_system.json");
+    export_data(b, "solar_system.json", "begin_sys");
 
     simulation(b, t, E, "energy.dat");
+    export_data(b, "solar_system.json", "end_sys");
 
     return 0;
 }
